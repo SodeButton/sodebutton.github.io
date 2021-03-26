5903dfa99d7e1e2c82948d6d1c155c481832a55c
SodeButton <takuya.20020824@gmail.com>
2021-03-26 16:38:09 +0900

    update

--- docs/apps/maze/main.js
+++ docs/apps/maze/main.js
@@ -14,6 +14,10 @@ const game_width = 32 * maze_size;
 const game_height = 32 * maze_size;
 
 let maze = [];
+let timer = 0;
+let x = 2,
+    y = 2;
+let complete = false;
 
 class GameScene extends Phaser.Scene {
     constructor() {
@@ -40,36 +44,6 @@ class GameScene extends Phaser.Scene {
                 maze[i][j] = 1;
             }
         }
-
-        for (let i = 2; i < maze_size - 2; i += 2) {
-            for (let j = 2; j < maze_size - 2; j += 2) {
-                let dx = 0,
-                    dy = 0;
-                do {
-                    let rand = Math.floor(Math.random() * 3);
-                    if (i === 2) rand = Math.floor(Math.random() * 4);
-                    switch (rand) {
-                        case 0:
-                            dx = 0;
-                            dy = 1;
-                            break;
-                        case 1:
-                            dx = 0;
-                            dy = -1;
-                            break;
-                        case 2:
-                            dx = 1;
-                            dy = 0;
-                            break;
-                        case 3:
-                            dx = -1;
-                            dy = 0;
-                            break;
-                    }
-                } while (maze[i + dx][j + dy] !== 0);
-                maze[i + dx][j + dy] = 1;
-            }
-        }
         this.draw();
     }
 
@@ -85,7 +59,54 @@ class GameScene extends Phaser.Scene {
         }
     }
 
-    update(time, delta) {}
+    update(time, delta) {
+        timer += delta / 1000;
+        if (timer >= 0.2 && !complete) {
+            timer = 0;
+            let dx = 0,
+                dy = 0;
+            do {
+                let rand = Math.floor(Math.random() * 3);
+                if (x === 2) rand = Math.floor(Math.random() * 4);
+                switch (rand) {
+                    case 0:
+                        dx = 0;
+                        dy = 1;
+                        break;
+                    case 1:
+                        dx = 0;
+                        dy = -1;
+                        break;
+                    case 2:
+                        dx = 1;
+                        dy = 0;
+                        break;
+                    case 3:
+                        dx = -1;
+                        dy = 0;
+                        break;
+                }
+            } while (maze[x + dx][y + dy] !== 0);
+            maze[x + dx][y + dy] = 1;
+            if (maze[x + dx][y + dy] === 0) {
+                this.add.sprite(
+                    (x + dx) * 32 + 16,
+                    (y + dy) * 32 + 16,
+                    "ground"
+                );
+            } else if (maze[x + dx][y + dy] === 1) {
+                this.add.sprite((x + dx) * 32 + 16, (y + dy) * 32 + 16, "wall");
+            }
+            y += 2;
+            if (y > maze_size - 2) {
+                y = 2;
+                x += 2;
+                if (x > maze_size - 2) {
+                    complete = true;
+                }
+            }
+        }
+    }
 }
 
 const config = {
